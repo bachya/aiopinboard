f44395d9dc5f19104b462d85078c07b56166c265
Aaron Bach <bachya1208@gmail.com>
2020-09-03 16:12:40 -0600

    Fix example

--- examples/test_api.py
+++ examples/test_api.py
@@ -2,8 +2,8 @@
 import asyncio
 import logging
 
-from aiopinboard import Client
-from aiopinboard.errors import GuardianError
+from aiopinboard import API
+from aiopinboard.errors import RequestError
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,28 +12,13 @@ async def main() -> None:
     """Create the aiohttp session and run the example."""
     logging.basicConfig(level=logging.INFO)
 
-    async with Client("172.16.11.208") as guardian:
-        try:
-            wifi_status_response = await guardian.wifi.status()
-            _LOGGER.info("wifi_status command response: %s", wifi_status_response)
+    api = API("<PINBOARD_API_TOKEN>")
 
-            # wifi_reset_response = await guardian.wifi.reset()
-            # _LOGGER.info("wifi_reset command response: %s", wifi_reset_response)
-
-            # wifi_configure_response = await guardian.wifi.configure(
-            #     "<SSID>", "<PASSWORD>"
-            # )
-            # _LOGGER.info("wifi_configure command response: %s", wifi_configure_response)
-
-            # wifi_enable_ap_response = await guardian.wifi.enable_ap()
-            # _LOGGER.info("wifi_enable_ap command response: %s", wifi_enable_ap_response)
-
-            # wifi_disable_ap_response = await guardian.wifi.disable_ap()
-            # _LOGGER.info(
-            #     "wifi_disable_ap command response: %s", wifi_disable_ap_response
-            # )
-        except GuardianError as err:
-            _LOGGER.info(err)
+    try:
+        last_change_dt = await api.async_get_last_change_datetime()
+        _LOGGER.info(last_change_dt)
+    except RequestError as err:
+        _LOGGER.info(err)
 
 
 asyncio.run(main())
