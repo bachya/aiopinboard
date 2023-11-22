ff73f4bb8cffc4123c2e2a558fbfc682d536465f
Joseph Chiocchi <input.output@gmail.com>
2023-11-22 00:01:44 +0000

    align original formatting

--- tests/test_bookmark_api.py
+++ tests/test_bookmark_api.py
@@ -80,6 +80,7 @@ async def test_get_all_bookmarks(aresponses: ResponsesMockServer) -> None:
 
     async with ClientSession() as session:
         api = API(TEST_API_TOKEN, session=session)
+
         # The test's fixture static date for the returned bookmark.
         fixture_bookmark_date = datetime.strptime("2020-09-02T03:59:55Z", "%Y-%m-%dT%H:%M:%SZ")
         bookmarks = await api.bookmark.async_get_all_bookmarks(
